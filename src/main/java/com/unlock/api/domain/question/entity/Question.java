7ffdbe25ad20ea8b415567d86329d6e6f6ccc80f
LeeChangHyeong <chxngbro@gmail.com>
2026-02-04 20:18:48 +0900

    [feat] 질문 카테고리(QuestionCategory) 기능 추가

--- src/main/java/com/unlock/api/domain/question/entity/Question.java
+++ src/main/java/com/unlock/api/domain/question/entity/Question.java
@@ -15,24 +15,54 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+
+
 /**
+
  * 매일 제공되는 질문 엔티티
+
  */
+
 @Entity
+
 @Table(name = "questions")
+
 @Getter
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 @AllArgsConstructor
+
 @Builder
+
 public class Question extends BaseTimeEntity {
 
+
+
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
+
+
     @Column(nullable = false, columnDefinition = "TEXT")
+
     private String content; // 질문 내용
 
+
+
+    @Enumerated(EnumType.STRING)
+
+    @Column(nullable = false)
+
+    private QuestionCategory category; // 질문 카테고리
+
+
+
     @Column(nullable = false, unique = true)
+
     private LocalDate activeDate; // 질문이 공개되는 날짜
+
 }
