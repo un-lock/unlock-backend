74e962f9e106cd5a322e129a9b0fe38f622d237e
LeeChangHyeong <chxngbro@gmail.com>
2026-02-04 15:58:09 +0900

    [feat] 기본 애플리케이션 및 테스트 클래스 추가

---
+++ src/test/java/com/unlock/api/UnlockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
