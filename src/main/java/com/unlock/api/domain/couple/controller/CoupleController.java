5d21d14fdbfc2f43ea0571cd11a1d96cae9abf15
LeeChangHyeong <chxngbro@gmail.com>
2026-02-04 20:54:42 +0900

    [refactor] 커플 컨트롤러 인증 방식 개선 (@CurrentUser 적용)

--- src/main/java/com/unlock/api/domain/couple/controller/CoupleController.java
+++ src/main/java/com/unlock/api/domain/couple/controller/CoupleController.java
@@ -1,6 +1,7 @@
 package com.unlock.api.domain.couple.controller;
 
 import com.unlock.api.common.dto.ApiResponse;
+import com.unlock.api.common.security.annotation.CurrentUser;
 import com.unlock.api.domain.couple.dto.CoupleDto.ConnectRequest;
 import com.unlock.api.domain.couple.dto.CoupleDto.CoupleResponse;
 import com.unlock.api.domain.couple.service.CoupleService;
@@ -9,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +22,18 @@ public class CoupleController {
 
     /**
      * 내 커플 정보 및 초대 코드 조회
-     * TODO: SecurityContext에서 userId를 가져오도록 수정 필요
      */
     @GetMapping("/me")
-    public ApiResponse<CoupleResponse> getMyCoupleInfo(@RequestHeader("X-USER-ID") Long userId) {
+    public ApiResponse<CoupleResponse> getMyCoupleInfo(@CurrentUser Long userId) {
         return ApiResponse.success("커플 정보 조회 성공", coupleService.getCoupleInfo(userId));
     }
 
     /**
      * 초대 코드로 커플 연결
-     * TODO: SecurityContext에서 userId를 가져오도록 수정 필요
      */
     @PostMapping("/connect")
     public ApiResponse<Void> connectCouple(
-            @RequestHeader("X-USER-ID") Long userId,
+            @CurrentUser Long userId,
             @RequestBody @Valid ConnectRequest request) {
         coupleService.connect(userId, request.getInviteCode());
         return ApiResponse.success("커플 연결 성공", null);
