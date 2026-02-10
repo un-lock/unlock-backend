3cb90bcf4bd5a0706c2a5c90c893511585c67a1f
LeeChangHyeong <chxngbro@gmail.com>
2026-02-10 14:21:39 +0900

    [infra] Querydsl 환경 설정 및 빌드 최적화 (검증된 버전 조합 적용)

---
+++ src/main/java/com/unlock/api/common/config/QuerydslConfig.java
@@ -0,0 +1,23 @@
+package com.unlock.api.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Querydsl 설정 클래스
+ * JPAQueryFactory를 빈으로 등록하여 Repository 계층에서 Querydsl을 사용할 수 있게 합니다.
+ */
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
