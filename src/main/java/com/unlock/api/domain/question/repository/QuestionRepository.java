57c89af7e52e48d123f8c0581e317c117cb44cd1
LeeChangHyeong <chxngbro@gmail.com>
2026-02-10 14:21:42 +0900

    [refactor] QuestionRepository를 Querydsl 구조로 리팩토링 (랜덤 추출 로직 고도화)

--- src/main/java/com/unlock/api/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/unlock/api/domain/question/repository/QuestionRepository.java
@@ -7,13 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    
-    /**
-     * 특정 커플이 아직 배정받지 않은 모든 질문들 중 랜덤하게 하나를 가져옵니다.
-     */
-    @Query(value = "SELECT * FROM questions q WHERE q.id NOT IN " +
-                   "(SELECT cq.question_id FROM couple_questions cq WHERE cq.couple_id = :coupleId) " +
-                   "ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
-    Optional<Question> findRandomQuestionNotAssignedToCouple(@Param("coupleId") Long coupleId);
+public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+
 }
